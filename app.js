9deec2ab59bae6f0e3a41263d38e19ed94ad6e84
gfx96 <vasilacheandrei46@gmail.com>
2019-01-05 14:56:47 +0200

    Alfa version

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const express = require('express'),
-    reqProm = require('request-promise'),
-    fs = require('fs');
-
+const express = require('express');
 const app = express();
+
 const data = require('./manipulateData');
-const frontEnd = require('./manipulateFrontEnd');
 
 const port = 8080;
 const url = 'https://assist-software.net/testimonials';
 
 
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/Public'));
 
 
 app.get("/", (req, res) => {
-    data.getDataFromAllPages(url).then(
-        () => {
-            res.sendFile(__dirname + '/views/index.html');
-            data.removeRedundantDataFromCountries();
-            data.sortObjectArray();
-            data.numberOfTestmonialsByCountry();
-            console.log(data.testimonials.length);
-        }
-    );
+    if (data.testimonials.length == 0) {
+        data.getDataFromAllPages(url).then(
+            () => {
+                data.appFlow();
+                indexRender(res);
+            })
+    } else {
+        indexRender(res);
+    }
 });
+
+function indexRender(res) {
+    res.render('index', {
+        countries: data.getCounteris(),
+        testimonials: data.testimonials,
+        numberOfTestiminialsPerCountry: data.countryNumberOfTestimonials
+    });
+}
 
 
 app.listen(port, () => {
--- manipulateData.js
+++ manipulateData.js
@@ -1,3 +1,13 @@
+/*
+    '6. If an author belongs to multiple countries, display him under each country'
+
+     On website the maximum number of countries that an author can belong is 2,
+    so I used string manipulation instead of an array,for simplicity.
+    See line 101 and 102.
+
+*/
+
+
 const $ = require('cheerio'),
     reqProm = require('request-promise');
 
@@ -5,6 +15,10 @@ const $ = require('cheerio'),
 var testimonials = [];
 var countries = [];
 var countryNumberOfTestimonials = [];
+
+function getCounteris() {
+    return countries.slice();
+}
 
 function pushPersonToArray(name, profileImg, country) {
     testimonials.push({
@@ -15,7 +29,7 @@ function pushPersonToArray(name, profileImg, country) {
 }
 
 async function getDataFromAllPages(url) {
-    await getHtmlData(url)
+    await getHtmlData(url);
     for (var i = 1; i < 4; i++) {
         let exentedUrl = '?page=' + i;
         await getHtmlData(url + exentedUrl);
@@ -25,54 +39,86 @@ async function getDataFromAllPages(url) {
 async function getHtmlData(url) {
     await reqProm(url)
         .then((html) => {
-            let numberOfElements = $('div .testimonial-author > .testimonial-author ', html).length;
+            let numberOfElementsPerRequest = $('div .testimonial-author > .testimonial-author ', html).length;
             let author = $('div .testimonial-author > .testimonial-author ', html);
-            let country = $('.field-content > .testimonial-author >  .testimonial-country > .country', html);
-            let porfileImg = $('.field-content > .testimonial-author > p > a > img', html);
+            let country = $('div .field-content > .testimonial-author >  .testimonial-country ', html);
+            let profileImg = $('.field-content > .testimonial-author > p > a > img', html);
 
-            for (let i = 0; i < numberOfElements; i++) {
-                pushPersonToArray(author[i].children[0].data, porfileImg[i].attribs.src, country[i].children[0].data)
-            }
+            pushDataToTestimonialsArray(author, profileImg, country, numberOfElementsPerRequest);
         })
         .catch((err) => {
             console.log(err);
         })
 }
 
+function pushDataToTestimonialsArray(author, profileImg, country, numberOfElementsPerRequest) {
+    for (let i = 0; i < numberOfElementsPerRequest; i++) {
+        countries.push(country[i].children[1].children[0].data);
+        let countryElements = country[i].children[1].children[0].data + secondCountryValidator(country[i].children[4])
+        pushPersonToArray(author[i].children[0].data, profileImg[i].attribs.src, countryElements)
+    }
+}
+
+function secondCountryValidator(secondCountry) {
+    if (secondCountry !== undefined) {
+        return ' ' + secondCountry.children[0].data;
+    } else {
+        return '';
+    }
+}
 
 function removeRedundantDataFromCountries() {
     let unique = {};
-    testimonials.forEach((i) => {
-        if (!unique[i.country]) {
-            unique[i.country] = true;
-            countries.push(i.country);
+    let pivotArray = [];
+
+    countries.forEach((i) => {
+        if (!unique[i]) {
+            unique[i] = true;
         }
     });
+    pivotArray.push(Object.getOwnPropertyNames(unique));
+    countries.length = 0;
+    countries = pivotArray[0]
+
 }
 
 function sortObjectArray() {
-    testimonials.sort(function (a, b) {
-        let x = a.country.toUpperCase();
-        let y = b.country.toUpperCase();
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    countries.sort(function (a, b) {
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
     });
 }
 
-function numberOfTestmonialsByCountry() {
+function numberOfTestimonialsByCountry() {
+    let countryTotalNumber;
+
     for (let i = 0; i < countries.length; i++) {
-        let countryTotalNumber = testimonials.filter(country => country.country === countries[i]);
+
+        countryTotalNumber = testimonials.filter(
+            testimonial =>
+            countries[i] === testimonial.country[0] + testimonial.country[1] ||
+            countries[i] === testimonial.country[3] + testimonial.country[4]
+        );
+
         countryNumberOfTestimonials.push({
             country: countries[i],
-            numberOfTestimonials: countryTotalNumber.length - 1
+            numberOfTestimonials: countryTotalNumber.length
         });
+
     }
+}
+
+function appFlow() {
+    removeRedundantDataFromCountries();
+    sortObjectArray();
+    numberOfTestimonialsByCountry();
 }
 
 module.exports = {
     testimonials,
     countries,
+    countryNumberOfTestimonials,
+
+    getCounteris,
     getDataFromAllPages,
-    removeRedundantDataFromCountries,
-    sortObjectArray,
-    numberOfTestmonialsByCountry
+    appFlow
 }
